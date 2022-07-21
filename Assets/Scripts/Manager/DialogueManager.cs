7924cdf4ff848e368b0cd82c8147536b91ac764b
AsickAsack <junwoo314@gmail.com>
2022-07-21 21:49:55 +0900

    채팅작업중

---
+++ Assets/Scripts/Manager/DialogueManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    static DialogueManager Instance;
+    public Canvas Dialogue_Canavs;
+    public Text Dialouge_Name;
+    public Text Dialogue; 
+
+
+    private void Awake() {
+        
+        //텍스트 파일 로드하기
+        //    Resources.Load();
+
+    }
+
+    
+
+}
